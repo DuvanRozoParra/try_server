cfbba4fd5fe59849d76345e7c1ff609873dd505f
macrolomania <113312174+macrolomania@users.noreply.github.com>
2025-02-10 17:32:36 -0500

    feaat: cors

--- internal/manager.go
+++ internal/manager.go
@@ -3,6 +3,7 @@ package network
 import (
 	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func ServerVR() *fiber.App {
@@ -11,6 +12,12 @@ func ServerVR() *fiber.App {
 		ServerHeader: "ServerUnimeta",
 		AppName:      "Cbtic",
 	})
+
+	// Configurar CORS correctamente
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*", // Permite todas las conexiones
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	// Initialize default config (Assign the middleware to /metrics)
 	// app.Get("/metrics", monitor.New())
